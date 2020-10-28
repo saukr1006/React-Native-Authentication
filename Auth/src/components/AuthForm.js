e0d3191ca15171e29d8fe13c5f82fdcba0563906
Siddhartha Tiwari <201851127@iiitvadodara.ac.in>
2020-10-28 09:45:18 +0530

    Added comp and context

---
+++ Auth/src/components/AuthForm.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { StyleSheet } from "react-native";
+import { Text, Input, Button } from "react-native-elements";
+import Spacer from "../components/Spacer";
+
+const AuthForm = ({ headerText, errorMessage, onSubmit, submitButtonText }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <>
+      <Spacer>
+        <Text h3>{headerText}</Text>
+      </Spacer>
+      <Input
+        autoCapitalize="none"
+        autoCorrect={false}
+        label="Email"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <Spacer />
+      <Input
+        secureTextEntry
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={password}
+        onChangeText={setPassword}
+        label="Password"
+      />
+      {errorMessage ? (
+        <Text style={styles.errorMessage}>{errorMessage}</Text>
+      ) : null}
+      <Spacer>
+        <Button
+          title={submitButtonText}
+          onPress={() => onSubmit({ email, password })}
+        />
+      </Spacer>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  errorMessage: {
+    fontSize: 16,
+    color: "red",
+    marginLeft: 15,
+    marginTop: 15,
+  } 
+});
+
+export default AuthForm;
