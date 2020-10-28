4f376e1f04a71dd68b1f106b41874da3ea2497db
Siddhartha Tiwari <201851127@iiitvadodara.ac.in>
2020-10-28 09:42:59 +0530

    Added the entry js code

---
+++ Auth/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import AccountScreen from "./src/screens/AccountScreen";
+import SigninScreen from "./src/screens/SigninScreen";
+import SignupScreen from "./src/screens/SignupScreen";
+import HomeScreen from "./src/screens/HomeScreen";
+import {Provider as AuthProvider} from './src/context/AuthContext';
+import {setNavigator} from './src/navigationRef';
+import ResolveAuthScreen from './src/screens/ResolveAuthScreen';
+
+const switchNavigator = createSwitchNavigator({
+  ResolveAuth:ResolveAuthScreen,
+  loginFlow: createStackNavigator({
+    Signup: SignupScreen,
+    Signin: SigninScreen,
+  }),
+  mainFlow: createBottomTabNavigator({
+    Home: HomeScreen,
+    Account: AccountScreen,
+  }),
+});
+
+const App = createAppContainer(switchNavigator);
+export default () =>{
+  return (
+    <AuthProvider>
+      <App ref={(navigator) => {setNavigator(navigator)}}/>
+    </AuthProvider>
+  );
+};
